ec283d40b2788bcb0471b6d9866b2ce1057c69de
Omarr-Kh <omar1752003@gmail.com>
2023-03-21 11:56:58 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet
+ * Return: 0 always (success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j = 10;
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 26; i++)
+       		{
+			putchar(i + 97);
+		}
+		_putchar('\n');
+	}
+}
