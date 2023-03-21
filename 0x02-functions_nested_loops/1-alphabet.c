39455c15e1befddf820ce262af86367969fec976
Omarr-Kh <omar1752003@gmail.com>
2023-03-21 11:45:45 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+/**
+ * main - prints the alphabet
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(i + 97);
+	}
+
+	_putchar('\n');
+	return (0);
+}
