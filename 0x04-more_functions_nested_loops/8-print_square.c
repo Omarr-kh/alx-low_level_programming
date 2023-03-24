d7956c6402d70b7ffa98462bd2885bcd1a0086d7
Omarr-Kh <omar1752003@gmail.com>
2023-03-24 01:31:55 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square -prints das
+ * @n: number of char
+ */
+void print_square(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar('#');
+		}
+	}
+
+	_putchar('\n');
+}
