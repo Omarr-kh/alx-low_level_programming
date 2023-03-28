20d2152f88ab57c8ae6666a2ce987516b96d532a
Omarr-Kh <omar1752003@gmail.com>
2023-03-28 04:50:05 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,10 +15,10 @@ void puts_half(char *str)
 	if (i % 2 == 0)
 		n = i / 2;
 	else
+	{
 		n = (i - 1) / 2;
-	
-	/*printf("%d \n %d", i, n);
-	_putchar('\n');*/
+		n += 1;
+	}
 	for (j = n; j < i; j++)
 		_putchar(str[j]);
 	_putchar('\n');
