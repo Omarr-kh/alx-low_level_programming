b03f29b4842f831195389df6dd3d07a82f5986da
Omarr-Kh <omar1752003@gmail.com>
2023-04-09 05:20:12 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ * @argc: count of args
+ * @argv: args vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
