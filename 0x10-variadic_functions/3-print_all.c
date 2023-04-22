65e71e8aeccc9db8941e0a407a954727424023bb
Omarr-Kh <omar1752003@gmail.com>
2023-04-21 17:00:56 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -26,6 +26,9 @@ void print_all(const char * const format, ...)
 				case 'i':
 					printf("%s%d", sep, va_arg(ap, int));
 					break;
+				case 'f'"
+					printf("%s%f", sep, va_arg(ap, double));
+					break;
 				case 's':
 					str = va_arg(ap, char *);
 					if (str == NULL)
