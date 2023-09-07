a19f7ab5e12ebe23906c50a289e5c72bb3e38d6a
Omarr-kh <omar1752003@gmail.com>
2023-09-07 10:36:25 -0700

    task 1

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - function that compute hash of 'str' using djb2
+ *
+ * @str: string to be hashed
+ * Return: value of hash
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
