0019a3cf8971e152049187673faa2913ad1576e5
Omarr-kh <omar1752003@gmail.com>
2023-09-07 10:42:54 -0700

    task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function returns the index of a key
+ *
+ * @key: key to hash
+ * @size: size of hash table
+ * Return: index of the key in the table
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
